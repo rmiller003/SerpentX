00fa08d77885911f8dfc7e4e4065dbae23108ce1
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2020-09-24 22:20:33 -0400

    Commit 1.24

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 # This is my 4th attempt at the classic Python Snake Game...
 
-import pygame, sys
 
-pygame.init()
+def main():
+    game = Game()
+    game.run()
+    sys.exit()
 
-DISPLAYSURF = pygame.display.set_mode((800, 600))
-#BASICFONT = pygame.font.Font('freesansbold.ttf')
-CLOCK = pygame.time.Clock()
-pygame.display.set_caption('SerpentX')
+    if __name__ == '__main__':
+        main()
 
-while True: #main game loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_ESCAPE:
-            pygame.quit()
-            sys.exit()
-
-    DISPLAYSURF.fill((255,255,255))
-    pygame.display.update()
-    CLOCK.tick(60)
-
-
-#--------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------
 # draw window
 # update the game logic
 # handle input
